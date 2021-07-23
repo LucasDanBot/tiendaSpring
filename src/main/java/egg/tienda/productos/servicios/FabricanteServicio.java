1dfb7710874ca138f33dc592fd234a3fee080145
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-07-23 17:19:18 -0300

    Update FabricanteServicio.java

--- src/main/java/egg/tienda/productos/servicios/FabricanteServicio.java
+++ src/main/java/egg/tienda/productos/servicios/FabricanteServicio.java
@@ -1,9 +1,47 @@
 package egg.tienda.productos.servicios;
 
+import egg.tienda.productos.entidades.Fabricante;
+import egg.tienda.productos.errores.ErrorServicio;
+import egg.tienda.productos.repositorios.FabricanteRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FabricanteServicio {
     
+    @Autowired
+    private FabricanteRepositorio fabricanteRepositorio;
     
     
     
+    public void crearFabricante (String nombre) throws ErrorServicio {
+        
+        
+        // hacer validaciones!!!
+        
+        
+        validar(nombre);
+        
+        Fabricante fabricante = new Fabricante();
+        
+        fabricante.setNombre(nombre);
+        
+        fabricanteRepositorio.save(fabricante);
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    private void validar (String nombre) throws ErrorServicio{
+        
+        if (nombre == null || nombre.isEmpty()) {
+            throw new ErrorServicio("El nombre del Autor esta vacio");
+        }
+        
+    }
     
 }
