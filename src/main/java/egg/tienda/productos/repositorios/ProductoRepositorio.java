7bce6efd378a6f78093bf6e97db120c0369f6d0a
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-07-26 21:41:03 -0300

    Update ProductoRepositorio.java

--- src/main/java/egg/tienda/productos/repositorios/ProductoRepositorio.java
+++ src/main/java/egg/tienda/productos/repositorios/ProductoRepositorio.java
@@ -2,7 +2,9 @@ package egg.tienda.productos.repositorios;
 
 import egg.tienda.productos.entidades.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductoRepositorio extends JpaRepository<Producto, String>{
     
     
