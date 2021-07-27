b5231aedca7e2cf7b615df87e5490508ee7b2265
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-07-27 18:55:29 -0300

    Update ProductoServicio.java

--- src/main/java/egg/tienda/productos/servicios/ProductoServicio.java
+++ src/main/java/egg/tienda/productos/servicios/ProductoServicio.java
@@ -1,8 +1,23 @@
 package egg.tienda.productos.servicios;
 
+import egg.tienda.productos.repositorios.ProductoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ProductoServicio {
     
+    @Autowired
+    private ProductoRepositorio prodcutoRepositorio;
     
+    
+    public void crearProducto (String nombre, Integer precio, String descripcion) {
+        
+        
+        
+        
+        
+        
+        
+    }
     
     
     
