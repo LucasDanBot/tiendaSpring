d514c674176e2e5f8418257b7579523fbc9a8daf
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-07-26 21:41:00 -0300

    Update FabricanteRepositorio.java

--- src/main/java/egg/tienda/productos/repositorios/FabricanteRepositorio.java
+++ src/main/java/egg/tienda/productos/repositorios/FabricanteRepositorio.java
@@ -4,7 +4,9 @@ import egg.tienda.productos.entidades.Fabricante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FabricanteRepositorio extends JpaRepository<Fabricante, String> {
     
     @Query("SELECT c FROM Fabricante c WHERE c.nombre = :nombre")
