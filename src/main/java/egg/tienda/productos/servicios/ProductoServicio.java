257edfa28d5fa046d0a0f6066bb49c4f4b1ee34e
LucasDanBot <72525638+LucasDanBot@users.noreply.github.com>
2021-07-28 11:15:51 -0300

    Update ProductoServicio.java

--- src/main/java/egg/tienda/productos/servicios/ProductoServicio.java
+++ src/main/java/egg/tienda/productos/servicios/ProductoServicio.java
@@ -1,5 +1,6 @@
 package egg.tienda.productos.servicios;
 
+import egg.tienda.productos.errores.ErrorServicio;
 import egg.tienda.productos.repositorios.ProductoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -21,4 +22,32 @@ public class ProductoServicio {
     
     
     
+    
+    
+    
+    
+    
+    private void validarNombre (String nombre, Integer precio, String descripcion) throws ErrorServicio{
+        
+        if (nombre == null || nombre.isEmpty()) {
+            throw new ErrorServicio("El nombre del Autor esta vacio");
+        }
+        
+    }
+    
+    private void validarPrecio (String nombre, Integer precio, String descripcion) throws ErrorServicio{
+        
+        if (nombre == null || nombre.isEmpty()) {
+            throw new ErrorServicio("El nombre del Autor esta vacio");
+        }
+        
+    }
+    
+    private void validarDescripcion (String nombre, Integer precio, String descripcion) throws ErrorServicio{
+        
+        if (nombre == null || nombre.isEmpty()) {
+            throw new ErrorServicio("El nombre del Autor esta vacio");
+        }
+        
+    }
 }
